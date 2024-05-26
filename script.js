bf2c4326fabe951d0e0f283edae0eef20c19200c
Ashish <145586793+ashishpathak1470@users.noreply.github.com>
2024-05-26 16:58:49 +0530

    dynamic routes, template engines

--- script.js
+++ script.js
@@ -7,14 +7,35 @@ app.use(function(req, res, next) {
     next();
 })
 
+app.use(function(req, res, next){
+  console.log("This is the middleware")
+  next();
+})
+
+// template engine
+app.set("view engine", "ejs");
 
 // routes
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('index', {age :12})
 })
+
+app.get('/contact', function (req, res) {
+  res.render('contact', {name : "ashish"})
+})
+
+
+
+
 
 app.get('/profile', function (req, res) {
     res.send('This is the profile page')
 })
+
+// dynamic routes
+app.get('/profile/:username', function (req, res) {
+  res.send(`This is the ${req.params.username} profile`)
+})
+
 
 app.listen(3000)
